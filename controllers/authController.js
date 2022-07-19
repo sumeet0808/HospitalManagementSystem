6671e2f29b927edd677d5ffce078925ed3d0d569
AwesomeABC <adityakhobragade05@gmail.com>
2022-07-19 11:53:16 +0530

    code compression

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,9 @@
-import { promisify } from "util";
-import jwt from "jsonwebtoken";
 import Patient from "../models/PatientModel.js";
 import Doctor from "../models/DoctorModel.js";
 import Admin from "../models/AdminModel.js";
 import { StatusCodes } from "http-status-codes";
 import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
+import login from "./loginController.js";
 
 const register = async (req, res) => {
   const { firstName, lastName, emailId, phoneNo, password, gender } = req.body;
@@ -43,114 +42,21 @@ const register = async (req, res) => {
 };
 
 const patientLogin = async (req, res, next) => {
-  const { emailId, password } = req.body;
-  try {
-    if (!emailId || !password) {
-      throw new BadRequestError("please provide all values");
-    }
-    const patient = await Patient.findOne({
-      emailId,
-    }).select("+password");
-    console.log("patient::::::", patient);
-    if (!patient) {
-      throw new UnAuthenticatedError("invalid cerdentials");
-    }
-    const isPasswordCorrect = await patient.comparePassword(password);
-    if (!isPasswordCorrect) {
-      throw new UnAuthenticatedError("Invalid Credentials");
-    }
-    const token = patient.createJWT();
-    patient.password = undefined;
-    res.status(StatusCodes.OK).json({
-      patient,
-      token,
-    });
-  } catch (error) {
-    next(error);
-  }
+  login(req, res, Patient, next)
 };
 
 const doctorLogin = async (req, res, next) => {
-  const { emailId, password } = req.body;
-  try {
-    if (!emailId || !password) {
-      throw new BadRequestError("please provide all values");
-    }
-    const doctor = await Doctor.findOne({
-      emailId,
-    }).select("+password");
-    if (!doctor) {
-      throw new UnAuthenticatedError("invalid cerdentials");
-    }
-    const isPasswordCorrect = await doctor.comparePassword(password);
-    if (!isPasswordCorrect) {
-      throw new UnAuthenticatedError("Invalid Credentials");
-    }
-    const token = doctor.createJWT();
-    doctor.password = undefined;
-    res.status(StatusCodes.OK).json({
-      doctor,
-      token,
-    });
-  } catch (error) {
-    next(error);
-  }
+  login(req, res, Doctor, next)
 };
 
 const adminLogin = async (req, res, next) => {
-  const { emailId, password } = req.body;
-  try {
-    if (!emailId || !password) {
-      throw new BadRequestError("please provide all values");
-    }
-    const admin = await Admin.findOne({
-      emailId,
-    }).select("+password");
-    if (!admin) {
-      throw new UnAuthenticatedError("invalid cerdentials");
-    }
-    const isPasswordCorrect = await admin.comparePassword(password);
-    if (!isPasswordCorrect) {
-      throw new UnAuthenticatedError("Invalid Credentials");
-    }
-    const token = admin.createJWT();
-    admin.password = undefined;
-    res.status(StatusCodes.OK).json({
-      admin,
-      token,
-    });
-  } catch (error) {
-    next(error);
-  }
+  login(req, res, Admin, next)
 };
 
-// const protect = async (req, res, next) => {
-//   // 1) Getting token and check of it's there
-//   let token;
-//   if (
-//     req.headers.authorization &&
-//     req.headers.authorization.startsWith("Bearer")
-//   ) {
-//     token = req.headers.authorization.split(" ")[1];
-//   }
+export {
+  register,
+  patientLogin,
+  doctorLogin,
+  adminLogin
+};
 
-//   if (!token) {
-//     return next("You are not logged in! Please log in to get access.", 401);
-//   }
-
-//   // 2) Verification token
-//   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-
-//   // 3) Check if user still exists
-//   const currentUser = await Admin.findById(decoded.userId);
-//   if (!currentUser) {
-//     return next("The user belonging to this token does no longer exist.", 401);
-//   }
-
-//   // GRANT ACCESS TO PROTECTED ROUTE
-//   req.user = currentUser;
-//   next();
-// };
-//export { register, login, protect };
-
-export { register, patientLogin, doctorLogin, adminLogin };
