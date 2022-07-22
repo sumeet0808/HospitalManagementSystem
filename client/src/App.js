933357af0f43b5e7c0608c12f16f285d05756d16
Dhanashri Patil <dhanashrip@excellonsoft.com>
2022-07-22 15:31:05 +0530

    changes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,24 +10,21 @@ import { Adminpanel } from "./components/admin";
 import Login from "./components/authentication/login";
 import Register from "./components/authentication/register";
 import Layout from "./components/Layout";
-import Header from "./components/Header";
-import HeaderAuth from "./components/authentication/HeaderAuth";
-import LogoutHandler from "./components/authentication/logout"
+import Header from "./components/HeaderComponents/Header";
+import HeaderAuth from "./components/HeaderComponents/HeaderAuth";
+import LogoutHandler from "./components/authentication/logout";
 
 function App() {
   return (
     <BrowserRouter>
-      {window.location.pathname !== "/" ? <Header /> : <HeaderAuth />}
-      <Routes>
-        <Route path="/" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/login" element={<LogoutHandler />} />
-      </Routes>
-
       <Layout>
-      <Routes>
+        {window.location.pathname !== "/" ? <Header /> : <HeaderAuth />}
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<LogoutHandler />} />
           <Route path="/" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/contact" element={<Contact />} />
@@ -38,7 +35,7 @@ function App() {
           <Route path="/Patient" element={<Patient />} />
           <Route path="/adminPanel" element={<Adminpanel />} />
         </Routes>
-      </Layout> 
+      </Layout>
     </BrowserRouter>
   );
 }
